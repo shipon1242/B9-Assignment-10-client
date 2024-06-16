e67a3fc8184d741b1a3a6695b05616b4973797c9
shipon1242 <hossainmohammadshipon@gamil.com>
2024-06-16 08:51:58 +0600

    card decoration is done

--- src/Components/Home/CraftCard.jsx
+++ src/Components/Home/CraftCard.jsx
@@ -1,6 +1,13 @@
-
+import { FaStar } from "react-icons/fa";
+import { FaBangladeshiTakaSign } from "react-icons/fa6";
 
 const CraftCard = ({ craft }) => {
+  const handleViewDetails= (_id)=>{
+    console.log(_id)
+
+  }
+
+
   console.log(craft)
   const { image, item_name, made_by, price,
     processing_time, rating, short_description, stock_status, subcategory_Name, _id, customization
@@ -9,16 +16,17 @@ const CraftCard = ({ craft }) => {
   return (
     <div className="card card-compact  bg-red-200 shadow-xl">
       <figure className="h-60 md:h-72"><img src={image} alt="Shoes" /></figure>
-      <div className="card-body">
-        
+      
+      <div className="card-body ">
+         <h2 className="text-black text-lg flex gap-1 items-center "> <span className="text-orange-500"><FaStar /></span> {rating} </h2>
         <div className="card-title flex justify-between  text-black ">
-          <h2>{item_name}</h2>
+          <h2 >{item_name}</h2>
           
-          <p className="flex justify-end gap-1"> <span className="text-xl">৳</span> {price}</p>
+          <p className="flex justify-end items-center gap-1"> <span className="text-xl"><FaBangladeshiTakaSign /></span> {price}</p>
         </div>
-        <p className="text-black text-base">{short_description}</p>
+        <p className="text-black text-lg opacity-75 ">{short_description}</p>
         <div className="card-actions justify-end">
-          <button className="btn w-full text-xl  btn-secondary "> View Details</button>
+          <button onClick={()=>handleViewDetails(_id)} className="btn w-full text-xl  btn-secondary "> View Details</button>
         </div>
       </div>
     </div>
